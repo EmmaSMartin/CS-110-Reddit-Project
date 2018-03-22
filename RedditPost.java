67e794a9f0fffac5791a6bb17ae9c8130ef38d4a
EmmaSMartin <36998511+EmmaSMartin@users.noreply.github.com>
2018-03-22 00:22:00 -0400

    RedditPost 1
    Initial Commit of RedditPost

---
+++ RedditPost.java
@@ -0,0 +1,19 @@
+
+public class RedditPost {
+	String datePosted;
+	int numUpvotes;
+	int numDownvotes;
+	String postType;
+	String attachedURL;
+	String message;
+	String title;
+
+	
+	RedditPost(String postname)
+	{
+		title=postname;
+	}
+
+}
+
+
